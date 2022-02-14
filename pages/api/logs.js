8b4a0605ad7ae516b06c030913a19cd2c44b762c
Elizar Pepino <jupenz@gmail.com>
2022-02-15 00:53:29 +0800

    fix logs

--- pages/api/logs.js
+++ pages/api/logs.js
@@ -4,19 +4,23 @@ import { faunaClient } from '../../helpers/fauna'
 // eslint-disable-next-line
 export default async (req, res) => {
   if (req.method == 'GET') {
-    let query = await faunaClient.query(
-      q.Map(
-        q.Paginate(q.Documents(q.Collection('logs'))),
-        q.Lambda((log) => q.Get(log)),
-      ),
-    )
-    res.status(200).json(
-      query.data.map(({ data: { server, date, guilds, players } }) => ({
-        server,
-        date,
-        winner: guilds[0]?.name,
-        mvp: players[0]?.name,
-      })),
-    )
+    try {
+      let query = await faunaClient.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('logs'))),
+          q.Lambda((log) => q.Get(log)),
+        ),
+      )
+      res.status(200).json(
+        query.data.map(({ data: { server, date, guilds, players } }) => ({
+          server,
+          date,
+          winner: guilds[0]?.name,
+          mvp: players[0]?.name,
+        })),
+      )
+    } catch (error) {
+      res.status(400).json({ error: error.message })
+    }
   }
 }
